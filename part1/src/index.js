e727143f5d9cef378b9af83c063e00cbf67a3d58
Lucas Varese <lvarese@technisys.com>
2020-01-10 13:06:42 -0300

    before exercise unicafe

--- part1/src/index.js
+++ part1/src/index.js
@@ -28,8 +28,27 @@ const Button = ({text, onClick}) => {
     );
 };
 
+const History = (props) => {
+    if(props.allClicks.length === 0) {
+        return (
+            <div>
+                the app is used by pressing the buttons
+            </div>
+        );
+    }
+
+    return (
+        <div>
+            button press history: {props.allClicks.join(' ')}
+        </div>    
+    );
+};
+
 const App = (props) => {
     const [counter, setCounter] = useState(0);
+    const [left, setLeft] = useState(0);
+    const [right, setRight] = useState(0);
+    const [allClicks, setAll] = useState([]);
     const name = 'Piotr';
     const age = 18;
 
@@ -48,14 +67,33 @@ const App = (props) => {
         1000
     ); */
 
+    const handleLeftClick = () => {
+        setLeft(left + 1);
+        setAll([...allClicks, 'left']);
+    };
+    
+    const handleRightClick = () => {
+        setRight(right + 1);
+        setAll([...allClicks, 'right']);
+    };
+
     return (
         <div>
             <h1>Greetings</h1>
             <Hello name={name} age={age} />
-            <Display counter={counter} />
-            <Button text='plus' onClick={setToValue(counter + 1)} />
-            <Button text='minus' onClick={setToValue(counter - 1)} />
-            <Button text='zero' onClick={setToValue(0)} />
+            <div style={{margin: "1em 0"}}>
+                <Display counter={counter} />
+                <Button text='plus' onClick={setToValue(counter + 1)} />
+                <Button text='minus' onClick={setToValue(counter - 1)} />
+                <Button text='zero' onClick={setToValue(0)} />
+            </div>
+            <div style={{margin: "1em 0"}}>
+                {left}
+                <Button onClick={handleLeftClick} text='left' />
+                <Button onClick={handleRightClick} text='right' />
+                {right}
+            </div>
+            <History allClicks={allClicks} />
         </div>
     );
 };
