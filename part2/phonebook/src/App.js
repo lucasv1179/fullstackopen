14ae0aa7162bfbd57ac0a25f46da6deea0796586
Lucas Varese <lvarese@technisys.com>
2020-01-13 14:13:02 -0300

    before 2.12 exercise start

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Persons from './components/Persons';
 import PersonsForm from './components/PersonsForm';
 import Filter from './components/Filter';
@@ -6,15 +7,18 @@ import './App.css';
 
 const App = () => {
 
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456' },
-        { name: 'Ada Lovelace', number: '39-44-5323523' },
-        { name: 'Dan Abramov', number: '12-43-234345' },
-        { name: 'Mary Poppendieck', number: '39-23-6423122' }
-    ]);
+    const [persons, setPersons] = useState([]);
     const [newName, setNewName] = useState('');
     const [newNumber, setNewNumber] = useState('');
     const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+        axios
+            .get('http://localhost:3001/persons')
+            .then(res => {
+                setPersons(res.data);
+            });
+    }, []);
 
     const handleNameInput = (event) => {
         setNewName(event.target.value);
