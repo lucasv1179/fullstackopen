a3c4daa33a948c32ba9f8b0202eddf8c39886a52
Lucas Varese <lvarese@technisys.com>
2020-01-13 13:45:33 -0300

    before exercise 2.11

--- part2/src/App.js
+++ part2/src/App.js
@@ -1,11 +1,23 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Note from './components/Note';
 
-const App = (props) => {
+const App = () => {
 
-    const [notes, setNotes] = useState(props.notes);
+    const [notes, setNotes] = useState([]);
     const [newNote, setNewNote] = useState('');
     const [showAll, setShowAll] = useState(true);
+
+    useEffect(() => {
+        console.log('effect');
+        axios
+            .get('http://localhost:3001/notes')
+            .then(res => {
+                console.log('promise fulfilled');
+                setNotes(res.data);
+            });
+    }, []);
+    console.log('render', notes.length, 'notes');
 
     const notesToShow = showAll
         ? notes
