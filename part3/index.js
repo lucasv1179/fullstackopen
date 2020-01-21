714a8862a22eaf2f4d49059004182e1b688affa0
Lucas Varese <lvarese@technisys.com>
2020-01-21 17:39:06 -0300

    After exercise 3.6

---
+++ part3/index.js
@@ -0,0 +1,90 @@
+//const http = require('http');
+const express = require('express'); //Replaced Node's built-in http module with express
+const bodyParser = require('body-parser'); //Express library
+
+let notes = [
+    {
+        id: 1,
+        content: "HTML is easy",
+        date: "2019-05-30T17:30:31.098Z",
+        important: true
+    },
+    {
+        id: 2,
+        content: "Browser can execute only Javascript",
+        date: "2019-05-30T18:39:34.091Z",
+        important: false
+    },
+    {
+        id: 3,
+        content: "GET and POST are the most important methods of HTTP protocol",
+        date: "2019-05-30T19:20:14.298Z",
+        important: true
+    }
+];
+
+/* const app = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(notes));
+}); */
+const app = express();
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Hello Node.js!</h1>');
+});
+
+app.get('/notes', (req, res) => {
+    res.json(notes);
+});
+
+app.get('/notes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const note = notes.find(note => note.id === id);
+    if (note) {
+        res.json(note);
+    } else {
+        res.status(404).end();
+    }
+});
+
+app.post('/notes', (req, res) => {
+    const body = req.body;
+    console.log(body);
+
+    if (!body.content) {
+        return res.status(400).json({
+            error: 'content missing'
+        });
+    }
+
+    const note = {
+        id: generateId(),
+        date: new Date(),
+        content: body.content,
+        important: body.important || false
+    };
+
+    notes = [...notes, note];
+
+    res.json(note);
+});
+
+app.delete('/notes/:id', (req, res) => {
+    const id = Number(req.params.id);
+    notes = notes.filter(note => note.id !== id);
+    res.status(204).end();
+});
+
+const port = 3001;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
+
+function generateId() {
+    const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0;
+    return maxId + 1;
+};
---
+++ part3/phonebook_backend/index.js
@@ -0,0 +1,113 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = 3001;
+
+app.use(bodyParser.json());
+
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Jane Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+];
+
+app.get('/api/persons', (req, res) => {
+    res.json(persons);
+});
+
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if (!person) {
+        return res.status(404).json({
+            error: 'resource not found'
+        });
+    }
+
+    res.json(person);
+});
+
+app.post('/api/persons', (req, res) => {
+    const body = req.body;
+    const reject = {};
+
+    if (!body.name && !body.number) {
+        reject.reason = 'name and number are missing';
+    } else if (!body.name) {
+        reject.reason = 'name is missing';
+    } else if (!body.number) {
+        reject.reason = 'number is missing';
+    } else {
+        const isNameInPhonebook = persons.some(person => person.name === body.name);
+        if (isNameInPhonebook) {
+            reject.reason = 'name must be unique';
+        }
+    }
+
+    if (reject.reason) {
+        return res.status(400).json({
+            error: reject.reason
+        });
+    }
+    
+    const person = {
+        id: createID(),
+        name: body.name,
+        number: body.number
+    };
+
+    persons = [...persons, person];
+
+    res.status(201).json(person);
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+    const person = persons.find(person => person.id === id);
+
+    if (!person) {
+        return res.status(404).json({
+            error: 'No person with that id exists'
+        });
+    } else {
+        persons = persons.filter(p => p !== person);
+        return res.status(204).end();
+    }
+});
+
+app.get('/info', (req, res) => {
+    const responseBody = `
+        <div>
+            <p>Phonebook has info for ${persons.length} people</p>
+            <p>${new Date()}</p>
+        </div>
+    `;
+    res.send(responseBody);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
+function createID() {
+    return Math.floor(Math.random() * 100000000);
+}
