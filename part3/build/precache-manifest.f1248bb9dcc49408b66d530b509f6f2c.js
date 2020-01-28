7300357ae80606712aa7a9bce30898e79eacf60b
Lucas Varese <lvarese@technisys.com>
2020-01-28 13:59:08 -0300

    before starting exercise 3.9

---
+++ part3/build/precache-manifest.f1248bb9dcc49408b66d530b509f6f2c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa02b86ce63cbae69cc17222e8a787ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "11d574635d5d3c7f20d9",
+    "url": "/static/css/main.20a6f418.chunk.css"
+  },
+  {
+    "revision": "b681b6810eab9d7032cc",
+    "url": "/static/js/2.da59dfb8.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.da59dfb8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "11d574635d5d3c7f20d9",
+    "url": "/static/js/main.8db10f0b.chunk.js"
+  },
+  {
+    "revision": "56a56afe9f2f9b69ee75",
+    "url": "/static/js/runtime-main.774383f0.js"
+  }
+]);
